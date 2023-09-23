8d4d86fd35643b085551432448016388ca5b9217
beekie18 <aviv@newmanatlanta.com>
2023-09-23 09:46:31 -0400

    Init

---
+++ AnimBody3D.gd
@@ -0,0 +1,12 @@
+extends AnimatableBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	rotate(Vector3.UP, PI/20)
+	translate(Vector3.UP * 0.05)
